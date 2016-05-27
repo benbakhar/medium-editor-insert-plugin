fbf6be30c2d98b7b8a8b7787a83a85749993409a
Ben Bakhar <ben.bakhar@apester.com>
2016-05-28 00:59:47 +0300

    fix upload handler to support our payload conventions

--- dist/js/medium-editor-insert-plugin.js
+++ dist/js/medium-editor-insert-plugin.js
@@ -1854,7 +1854,7 @@ this["MediumInsert"]["Templates"]["src/js/templates/images-toolbar.hbs"] = Handl
      */
 
     Images.prototype.uploadDone = function (e, data) {
-        $.proxy(this, 'showImage', data.result.files[0].url, data)();
+        $.proxy(this, 'showImage', window.configuration.imgixMediaFilterUrl + data.payload, data)();
 
         this.core.clean();
         this.sorting();
